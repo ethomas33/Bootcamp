78d4f907bba4888398e69b187e31638d8ae449de
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-02-10 21:58:39 -0500

    updates

--- 3-2-Student-Resources/07-Stu_ReadNetFlixCSV/Unsolved/netflix_ET.py
+++ 3-2-Student-Resources/07-Stu_ReadNetFlixCSV/Unsolved/netflix_ET.py
@@ -6,11 +6,18 @@ import csv
 video = input("What show or movie are you looking for? ")
 
 # Set path for file
-
+csvpath = os.path.join('..', 'Resources', 'netflix_ratings.csv')
 
 # Open the CSV
-
+with open(csvpath) as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
 
     # Loop through looking for the video
+    csv_header = next (csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+                
     
 
