a2bdf9de0046be7692b5a899fdf9410efa148cd0
Elizabeth Thomas <elizabeththomas33@gmail.com>
2022-04-08 13:29:32 -0400

    class

--- 11-2 Activities/3/Unsolved/index_starter.js
+++ 11-2 Activities/3/Unsolved/index_starter.js
@@ -1,15 +1,4 @@
 // Initialize the function
-var sentence =   {
-  I: 3,
-  always: 1,
-  and: 1,
-  be: 1,
-  what: 2,
-  will: 1,
-  yam: 3
-}
-function splitString(sentence) {
-// Mean is the average of all the values.
 
   // Convert string to an array of words
   
